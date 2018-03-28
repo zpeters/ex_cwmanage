56262a1c646c06800895356c77a718cf1262d5b4
Zach Peters <zpeters@gmail.com>
2018-03-28 18:48:10 -0500

    Added a env var and secret mechanism for configs

---
+++ config/dev.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+# if secret exists use that, otherwise
+# use env variables
+if File.exists?("config/dev.secret.exs") do
+  import_config "dev.secret.exs"
+else
+  config :ex_cwmanage,
+    cw_api_root: System.get_env("CW_API_ROOT"),
+    cw_companyid: System.get_env("CW_COMPANYID"),
+    cw_publickey: System.get_env("CW_PUBLICKEY"),
+    cw_privatekey: System.get_env("CW_PRIVATEKEY")
+end
+
+### APIs
+# Live ConnectWise api
+config :ex_cwmanage, :connectwise_api, ExCwmanage.Api.HTTPClient
