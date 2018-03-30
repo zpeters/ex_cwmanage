b826807b555c0fd632e1d6d3213e3d329ca61b7c
Zach Peters <zpeters@gmail.com>
2018-03-29 20:40:46 -0500

    Bumped timeout

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :ex_cwmanage,
-  http_timeout: 10_000,
-  http_recv_timeout: 10_000
+  http_timeout: 12_000,
+  http_recv_timeout: 12_000
 
 import_config "#{Mix.env()}.exs"
