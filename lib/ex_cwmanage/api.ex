d82fe8c68183636968c165176d46198d48b8880b
Joseph Banks <joseph@josephbanks.me>
2018-10-03 22:17:54 +0100

    Add more HTTP verbs

--- lib/ex_cwmanage/api.ex
+++ lib/ex_cwmanage/api.ex
@@ -3,15 +3,33 @@ defmodule ExCwmanage.Api do
   This is the main API logic.  Currently we only support GET with the 'conditions' as an option.
   Eventually this will encompass all of the HTTP verbs and the allowable ConnectWise 'parameters'
   """
+
+  @type path :: String.t()
+
   @connectwise_api Application.get_env(:ex_cwmanage, :connectwise_api)
 
-  @callback get(path :: String.t(), opts :: list()) :: %{}
+  @callback get(path :: path, opts :: list()) :: map
   def get(path, opts \\ []) do
     @connectwise_api.get(path, opts)
   end
 
-  @callback post(path :: String.t(), payload :: String.t()) :: %{}
+  @callback post(path :: path, payload :: String.t()) :: map
   def post(path, payload) do
     @connectwise_api.post(path, payload)
+  end
+
+  @callback put(path :: path, payload :: String.t()) :: map
+  def put(path, payload) do
+    @connectwise_api.put(path, payload)
+  end
+
+  @callback patch(path :: path, payload :: String.t()) :: map
+  def patch(path, payload) do
+    @connectwise_api.patch(path, payload)
+  end
+
+  @callback delete(path :: path, payload :: String.t()) :: map
+  def delete(path, payload) do
+    @connectwise_api.delete(path, payload)
   end
 end
