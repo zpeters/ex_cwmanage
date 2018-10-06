f13c729028a600c25decfe92ede7bb82863e7b9c
Zach Peters <zpeters@gmail.com>
2018-10-06 16:45:48 -0500

    Added a helper to get users and their emails

--- lib/ex_cwmanage/manage.ex
+++ lib/ex_cwmanage/manage.ex
@@ -4,6 +4,24 @@ defmodule ExCwmanage.Manage do
   """
 end
 
+defmodule ExCwmanage.Manage.Contacts do
+  import ExCwmanage.Api
+
+  def get_contacts_for_company(companyid) do
+    {:ok, contacts} = get('/company/contacts?conditions=company/identifier="#{companyid}"&fields=id,firstName,lastName,email')
+    contacts
+    |> Enum.map(&Map.put(&1, "email", get_email_for_contact(&1["id"])))
+  end
+
+  defp get_email_for_contact(contactid) do
+     {:ok, comms} = get('/company/contacts/#{contactid}/communications')
+    comms
+    |> Enum.filter(&Map.get(&1, "communicationType") == "Email")
+    |> Enum.map(&(Map.fetch!(&1, "value")))
+  end
+
+end
+
 defmodule ExCwmanage.Manage.System do
   import ExCwmanage.Api
 
