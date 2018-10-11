bb1d3d5e990875b423b804e9821e61e67c712cc6
Zach Peters <zpeters@gmail.com>
2018-10-10 21:40:01 -0500

    Updated elixir version to 1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExCwmanage.MixProject do
       app: :ex_cwmanage,
       name: "ExCwmanage",
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
