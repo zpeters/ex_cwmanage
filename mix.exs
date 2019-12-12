f09848751a219eca5602c932e9e65421b72690c6
Zach Peters <zpeters@gmail.com>
2019-12-12 15:44:40 -0600

    bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExCwmanage.MixProject do
     [
       app: :ex_cwmanage,
       name: "ExCwmanage",
-      version: "0.3.5",
+      version: "0.3.6",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
